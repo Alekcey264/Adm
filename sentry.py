0a3018afa29c4a735bd140e3e383729d5face828
Alekcey264 <alekseverevkin@gmail.com>
2023-05-19 13:10:44 +0500

    blabla

---
+++ sentry.py
@@ -0,0 +1,14 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://23930b3247784f6493f1b7d088a30f4e@o4505206914088960.ingest.sentry.io/4505209606701056",
+    traces_sample_rate=1.0
+)
+
+with sentry_sdk.configure_scope() as scope:
+    scope.set_tag('first tag','keep loading')
+    c = 2
+    try:
+        print(c/0)
+    except Exception:
+        sentry_sdk.capture_message("Exception")
